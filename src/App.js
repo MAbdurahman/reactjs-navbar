b586ea21459fc27ec3fa89543cd8714f98fceae8
MAbdurahman <mdbdrrhm@gmail.com>
2021-08-25 08:04:27 -0500

    Modify App.js - setup routes to pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Services from './pages/Services';
 
 export default function App() {
 	return (
-		<div>
-			<h1>App</h1>
-		</div>
+		<Router>
+			<main>
+				<Switch>
+					<Route path='/' exact>
+						<Home />
+					</Route>
+					<Route path='/about' exact>
+						<About />
+					</Route>
+					<Route path='/services' exact>
+						<Services />
+					</Route>
+					<Route path='/portfolio' exact>
+						<Portfolio />
+					</Route>
+					<Route path='/contact' exact>
+						<Contact />
+					</Route>
+          <Redirect to='/' />
+				</Switch>
+			</main>
+		</Router>
 	);
 }
